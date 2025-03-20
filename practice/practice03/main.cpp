a2cf167ffb8298753746a3c3a589fc92e78fdcbf
trayvonburl <txburl@ualr.edu>
2025-03-20 12:11:03 -0500

    got visual studio setup added some assignment stubs.

--- practice/practice03/main.cpp
+++ practice/practice03/main.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// practice03.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
--- projects/project01/main.cpp
+++ projects/project01/main.cpp
@@ -1,11 +1,11 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// project01.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello project!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
